8537ea1b47efb02406e42dcf07b8f404630714a8
Ing. Petr Jindra <el.mordo@gmail.com>
2019-09-07 11:52:14 +0200

    tests for a 'left hand operator' filter style

--- test/test_left_handed_style_filter.ts
+++ test/test_left_handed_style_filter.ts
@@ -1,8 +1,9 @@
 
 import { describe, it } from "mocha"
-import { assert } from "chai"
+import { assert, expect } from "chai"
 
 import { LeftHandedStyleFilter } from "../src/filter"
+import { Filter } from "../src/interfaces"
 
 
 describe("Left hand style filter", function ()
@@ -26,4 +27,59 @@ describe("Left hand style filter", function ()
             assert(builder.closeChar == close);
         });
     });
-})
+
+    describe("test build calls", function ()
+    {
+        const FIELD = "my_field";
+        const OPERATOR = "eq";
+        const STR_VALUE = "val"
+        const BOOL_VALUE = true;
+
+        function createInstance(): LeftHandedStyleFilter
+        {
+            return new LeftHandedStyleFilter();
+        }
+
+        function assertData(filters: string[], expected: string)
+        {
+            assert(filters.length == 1);
+            assert(filters[0] == expected, "Expected '" + expected + "' but '" + filters[0] + "' was given");
+        }
+
+        function doTest(field: string, operator: string, value?: any)
+        {
+            let f: Filter = {field: field, operator: operator, value: value};
+            let builder = createInstance();
+            let result = builder.build([f]);
+            let expected = field + "%5B" + operator + "%5D=" + value;
+
+            assertData(result, expected);
+
+        }
+
+        it("test fully specified filter", function ()
+        {
+            doTest(FIELD, OPERATOR, STR_VALUE);
+        });
+
+        it("test filter with boolearn value", function ()
+        {
+            doTest(FIELD, OPERATOR, BOOL_VALUE);
+        });
+
+        it("test filter with empty string", function ()
+        {
+            doTest(FIELD, OPERATOR, "");
+        });
+
+        it("test NULL value. The 'null' value should trigger error", function ()
+        {
+            expect(() => doTest(FIELD, OPERATOR, null)).throws();
+        });
+
+        it("test UNDEFINED value. The `undefined` value should trigger error", function ()
+        {
+            expect(() => doTest(FIELD, OPERATOR, undefined)).throws();
+        });
+    });
+});
